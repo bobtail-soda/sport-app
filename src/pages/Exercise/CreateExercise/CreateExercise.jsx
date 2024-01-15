0c4eca985cb4a282e095c6ebf77174fb8f094ff0
SenPatthamwan <sen.patthamwan@gmail.com>
2024-01-16 00:14:14 +0700

    Created: Create Exercise Activity page & Added Flowbite Tailwind Plugin

--- src/pages/Exercise/CreateExercise/CreateExercise.jsx
+++ src/pages/Exercise/CreateExercise/CreateExercise.jsx
@@ -1,8 +1,108 @@
 import React from 'react'
+import { 
+  Box,
+  Container,
+  Grid,
+  Typography,
+  ThemeProvider,
+} from "@mui/material"
+
+import createExercise from "/images/createExercise.jpg"
+import { theme } from "../../../theme"
+import { Link } from "react-router-dom"
 
 const CreateExercise = () => {
   return (
-    <div>CreateExercise</div>
+    <>
+      <ThemeProvider theme={ theme }>
+        <Container
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}> 
+          <Box sx={{
+                width: '872px',
+                textAlign: 'center',
+               }}>
+            <Typography variant="h5" component="h1" sx={{fontWeight: 'medium'}}>
+              Create Exercise Activity
+            </Typography>
+              
+              <div>
+                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                  Running
+                  <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
+                  </svg>
+                </button>
+                {/* Dropdown menu */}
+                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              
+
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <textarea type="text" placeholder="Type some caption here..."></textarea>
+  <a href="#">
+  <img className="rounded-t-lg" src={createExercise} alt="running" />
+  </a>
+  <textarea type="text" placeholder="Type some description here..."></textarea>
+  <div className="p-5">
+    <p>2/2/2023</p>
+    
+  </div>
+</div>
+
+<div>
+                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                  Suggest duration: 30 min
+                  <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
+                  </svg>
+                </button>
+                {/* Dropdown menu */}
+                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <button id="dropdownDefaultButton" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                Start 
+              </button>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
   )
 }
 
