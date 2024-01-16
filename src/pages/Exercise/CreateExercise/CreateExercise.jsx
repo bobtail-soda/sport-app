0cc3646a2e3e18ae4b1e67914c7865f3574f7a61
SenPatthamwan <sen.patthamwan@gmail.com>
2024-01-16 22:46:16 +0700

    Created: Create Exercise Activity page & Deleted Flowbite plugin

--- src/pages/Exercise/CreateExercise/CreateExercise.jsx
+++ src/pages/Exercise/CreateExercise/CreateExercise.jsx
@@ -1,11 +1,5 @@
-import React from 'react'
-import { 
-  Box,
-  Container,
-  Grid,
-  Typography,
-  ThemeProvider,
-} from "@mui/material"
+import React from "react"
+import { Box, Container, Typography, ThemeProvider } from "@mui/material"
 
 import createExercise from "/images/createExercise.jpg"
 import { theme } from "../../../theme"
@@ -14,91 +8,79 @@ import { Link } from "react-router-dom"
 const CreateExercise = () => {
   return (
     <>
-      <ThemeProvider theme={ theme }>
+      <ThemeProvider theme={theme}>
         <Container
           sx={{
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}> 
-          <Box sx={{
-                width: '872px',
-                textAlign: 'center',
-               }}>
-            <Typography variant="h5" component="h1" sx={{fontWeight: 'medium'}}>
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "872px",
+              textAlign: "center",
+            }}
+          >
+            <Typography
+              variant="h5"
+              component="h1"
+              sx={{ fontWeight: "medium",
+                    m:5,
+            }}
+            >
               Create Exercise Activity
             </Typography>
-              
-              <div>
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                  Running
-                  <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
-                  </svg>
-                </button>
-                {/* Dropdown menu */}
-                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                  </ul>
-                </div>
+            <select className="mb-10 outline-0 block w-full p-2.5 px-0.5 rounded-4xl bg-blue text-white pl-5 text-sm">
+              <option value="Running" className="">Running</option>
+              <option value="Weight training">Weight training</option>
+              <option value="Hike">Hike</option>
+              <option value="Yoga">Yoga</option>
+              <option value="Swimming">Swimming</option>
+              <option value="Bicycle ride">Bicycle ride</option>
+              <option value="Walking">Walking</option>
+            </select>
+            <div className="flex flex-col w-full bg-white border border-grey rounded-card">
+              <textarea
+                id="message"
+                rows="1"
+                className="placeholder-black outline-0 block p-5 w-full text-sm rounded-text-top border-b border-grey"
+                placeholder="Type some caption here..."
+              ></textarea>
+              <div className="flex justify-center">
+                <img className="w-80 h-full object-cover" src={createExercise} alt="The group of women are running" />
               </div>
-
-              
-
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-  <textarea type="text" placeholder="Type some caption here..."></textarea>
-  <a href="#">
-  <img className="rounded-t-lg" src={createExercise} alt="running" />
-  </a>
-  <textarea type="text" placeholder="Type some description here..."></textarea>
-  <div className="p-5">
-    <p>2/2/2023</p>
-    
-  </div>
-</div>
-
-<div>
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                  Suggest duration: 30 min
-                  <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
-                  </svg>
-                </button>
-                {/* Dropdown menu */}
-                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                  </ul>
-                </div>
+              <textarea
+                id="message"
+                rows="5"
+                className="placeholder-grey-dark outline-0 block p-5 w-full text-sm border-grey"
+                placeholder="Type some caption here..."
+              ></textarea>
+              <div className="text-sm text-grey-dark p-5 flex justify-end">
+                <p>2/2/2023</p>
               </div>
+            </div>
 
-              <button id="dropdownDefaultButton" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                Start 
-              </button>
+            <div className="flex justify-center">
+              <select className="mt-5 mb-5 outline-0 block w-fit p-2.5 px-0.5 rounded-4xl bg-blue text-white pl-5 text-sm">
+                <option value="Running" className="">Suggest duration: 30 min</option>
+                <option value="Weight training">Weight training</option>
+                <option value="Hike">Hike</option>
+                <option value="Yoga">Yoga</option>
+                <option value="Swimming">Swimming</option>
+                <option value="Bicycle ride">Bicycle ride</option>
+                <option value="Walking">Walking</option>
+              </select>
+            </div>
+
+            <button type="submit" 
+              className="mb-6 text-white rounded-4xl bg-pink text-lg w-full py-1 text-center"
+              >
+              <Link href="/create-new-password" color='primary.white' underline="none">
+                Start
+              </Link>
+            </button>
           </Box>
         </Container>
       </ThemeProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,29 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     colors: {
       'blue': '#6DA8E7',
       'pink': '#E76F6D',
       'black': '#000',
-      'white': '#fff'
+      'white': '#fff',
+      'grey': '#E2E2E2',
+      'grey-dark': '#9D9D9D'
     },
     extend: {
       borderRadius: {
       '4xl': '2rem',
-      'otp': '15px'
+      'otp': '15px',
+      'card' : '20px 20px 20px 20px',
+      'text-top': '20px 20px 0 0',
     }},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [    
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+],
 }
 
